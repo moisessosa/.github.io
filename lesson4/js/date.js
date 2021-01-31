80ef6e83ec91cf9a027dc7c427f2a1b3292c4dc5
moisessosa <rod20028@byui.edu>
2021-01-31 01:10:48 -0300

    finished the small screen

---
+++ lesson4/js/date.js
@@ -0,0 +1,14 @@
+function retornarFecha(){
+  var fecha;
+  fecha=new Date();
+  let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+  let month = monthNames[fecha.getMonth()];
+  var cadena = "Last Update "+fecha.getDate()+' '+ month/*(fecha.getMonth()+1)*/+' '+fecha.getFullYear();
+  return cadena;
+}
+function retornarUpdate(){
+    var fecha;
+    fecha = new Date();
+    return  fecha.getFullYear();
+
+}
