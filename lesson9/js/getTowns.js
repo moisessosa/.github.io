e02ed7b90676a89ff29e145b95496e2ddbe76e70
moisessosa <rod20028@byui.edu>
2021-03-06 02:51:38 -0300

    ya la pantalla mediana esta lista

--- lesson9/js/getTowns.js
+++ lesson9/js/getTowns.js
@@ -8,6 +8,7 @@ fetch(requestURL)
     const towns = jsonObject['towns'];
     for (let i = 0; i < towns.length; i++ ) {
         let card = document.createElement('section');
+        let contenedor = document.createElement('div');
         let h2 = document.createElement('h2');
         let h4 = document.createElement('h4');
         let p = document.createElement('p');
@@ -22,11 +23,21 @@ fetch(requestURL)
         let src =`img/${towns[i].photo}`;//very importan
         image.setAttribute('src', src);
         image.setAttribute('alt',"photo " + towns[i].name);
+        image.setAttribute('class', "left");
+       // image.setAttribute('class', "photoindex"); nononno
 
-        card.appendChild(h2);
-        card.appendChild(h4);
-        card.appendChild(p);
+       contenedor.appendChild(h2);
+       contenedor.appendChild(h4);
+       contenedor.appendChild(p);
+       card.appendChild(contenedor);
         card.appendChild(image);
+        card.setAttribute('class',"town");
+        contenedor.setAttribute('class', "data");
+        if(i%2==0){
+            image.setAttribute('class', "derecha");
+            contenedor.setAttribute('class', "data2");
+
+        }
 
         document.querySelector('div.towns').appendChild(card);
 }
