4d6f886b61bd2a8bc95da3c6d706ca6121cd8b30
moisessosa <rod20028@byui.edu>
2021-04-05 14:23:38 -0300

    comenzamos el proyecto

--- lesson11/lesson10/js/eventstowns.js
+++ lesson11/lesson10/js/eventstowns.js
@@ -18,6 +18,7 @@ function getEvents(town){
                 }
                 
                 document.getElementById('events').innerHTML = events;
+                    
                
         }
         }
